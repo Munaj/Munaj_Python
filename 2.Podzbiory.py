4ebab2dde5dff87f4419f50d590e9d9d41e34ffa
Jakub <kuba130300@gmail.com>
2021-11-16 00:04:28 +0100

    Pierwszy push z vs code

---
+++ 2.Podzbiory.py
@@ -0,0 +1,15 @@
+﻿
+given_set =[1,2,3]
+
+def compute_all_subset(arr):
+    allSubset = []
+    allSubset.append([])
+    for item in arr:
+        allSubsetLength = len(allSubset)
+        for i in range(0,allSubsetLength):
+            temp = allSubset[i].copy()
+            temp.append(item)
+            allSubset.append(temp)
+    return allSubset
+
+print(compute_all_subset(given_set))
