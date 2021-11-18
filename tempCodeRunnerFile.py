077e785d37bb2df1d81286de8cbfeefaf52f2cf0
Jakub <kuba130300@gmail.com>
2021-11-18 13:11:00 +0100

    Git

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,6 @@
-  # for x in indeksy:
-            #     if(temp != indeksy[x]):
+    def __or__(self,other):
+        if(self.percent >= other.percent):
+            return self.percent
+        else:
+            return other.percent
+        
